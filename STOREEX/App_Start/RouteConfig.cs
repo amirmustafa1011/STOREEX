5139e12436e9b6090ed5e5e85dd2dafb503f08f6
Amir <68057745+amirmustafa1011@users.noreply.github.com>
2026-09-12 13:26:09 +0200

    Add MVC routing

---
+++ STOREEX/App_Start/RouteConfig.cs
@@ -0,0 +1,14 @@
+using System.Web.Mvc;
+using System.Web.Routing;
+namespace STOREEX
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+            routes.MapRoute("default", "{controller}/{action}/{id}", new { controller="Home", action="Index", id=UrlParameter.Optional });
+        }
+    }
+}
